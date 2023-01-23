9334f92bd799ee7f9466f5ff052ba4e47498b3b1
s-cumpalovic <stefancumpalovic98@gmail.com>
2023-01-23 11:27:01 +0100

    Redirection on register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,12 +3,26 @@
 import { UserFormComponent } from "../../components/UserFormComponent";
 import { IUserRegister } from "../../types/user.type";
 import { authService } from "@/services/AuthService";
+import { ACCESS_TOKEN } from "../../utils/static";
+import { useEffect } from "react";
+import { getItem } from "../../utils/helpers";
+import { useRouter } from "next/navigation";
 
 export default function Register() {
   const handleSubmit = async (data: IUserRegister) => {
+    const router = useRouter();
+
     const response = await authService.register(data);
     console.log(response);
   };
+
+  useEffect(() => {
+    const token = getItem(ACCESS_TOKEN);
+    if (token) {
+      router.push("/");
+    }
+  });
+
   return (
     <div>
       <h1>
